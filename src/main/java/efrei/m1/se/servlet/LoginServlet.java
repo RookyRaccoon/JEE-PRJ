a9d9024f11510c9312aae3a2bf77024ecf7c5d3d
Camille Briand <camille.briand@efrei.net>
2019-10-15 11:28:48 +0200

    Implement LoginServlet class

--- src/main/java/efrei/m1/se/servlet/LoginServlet.java
+++ src/main/java/efrei/m1/se/servlet/LoginServlet.java
@@ -1,4 +1,24 @@
 package efrei.m1.se.servlet;
 
-public class LoginServlet {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LoginServlet extends HttpServlet {
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		this.handleRequest(req, res);
+	}
+
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		this.handleRequest(req, res);
+	}
+
+
+	private void handleRequest(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		this.getServletContext().getRequestDispatcher("").forward(req, res);
+	}
 }
