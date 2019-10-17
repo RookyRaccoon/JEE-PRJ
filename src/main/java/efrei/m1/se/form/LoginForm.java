27103ea911cc719a727b4ea9c22ef481a7b62747
AmiralBl3ndic <camille.briand@efrei.net>
2019-10-17 20:27:14 +0200

    Add login with default admin credentials to LoginForm.login method

--- src/main/java/efrei/m1/se/form/LoginForm.java
+++ src/main/java/efrei/m1/se/form/LoginForm.java
@@ -2,6 +2,8 @@ package efrei.m1.se.form;
 
 import javax.servlet.http.HttpServletRequest;
 
+import static efrei.m1.se.utils.Constants.*;
+
 public final class LoginForm {
 	private static final String USERNAME_FIELD = "username";
 	private static final String PASSWORD_FIELD = "password";
@@ -14,6 +16,13 @@ public final class LoginForm {
 		final String password = req.getParameter(PASSWORD_FIELD);
 
 		// TODO: log the user in using database entries;
+
+		if (username.equals(DEFAULT_ADMIN_USERNAME) && password.equals(DEFAULT_ADMIN_PASSWORD)) {
+			req.setAttribute("connectionFailed", false);
+			// TODO: set the session and redirect the user
+
+			return;
+		}
 
 		req.setAttribute("connectionFailed", true);
 	}
