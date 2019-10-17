b8196d6a086d9d772de940de859867ddf762e4cf
AmiralBl3ndic <camille.briand@efrei.net>
2019-10-17 20:17:08 +0200

    Add basic login functionality to LoginForm.login method This will require to be adjusted when the database is ready

--- src/main/java/efrei/m1/se/form/LoginForm.java
+++ src/main/java/efrei/m1/se/form/LoginForm.java
@@ -1,4 +1,20 @@
 package efrei.m1.se.form;
 
+import javax.servlet.http.HttpServletRequest;
+
 public final class LoginForm {
+	private static final String USERNAME_FIELD = "username";
+	private static final String PASSWORD_FIELD = "password";
+
+	private LoginForm() {}
+
+
+	public static void login(HttpServletRequest req) {
+		final String username = req.getParameter(USERNAME_FIELD);
+		final String password = req.getParameter(PASSWORD_FIELD);
+
+		// TODO: log the user in using database entries;
+
+		req.setAttribute("connectionFailed", true);
+	}
 }
