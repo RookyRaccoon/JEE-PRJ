a85a1e7f6fc7a69fd42c45fa0608533c6d09d64c
AmiralBl3ndic <camille.briand@efrei.net>
2019-10-17 19:36:57 +0200

    Add fields username and password to User class

--- src/main/java/efrei/m1/se/model/User.java
+++ src/main/java/efrei/m1/se/model/User.java
@@ -1,4 +1,63 @@
 package efrei.m1.se.model;
 
 public class User {
+
+	/**
+	 * Unique username allowing to identify a user in the database.
+	 */
+	private String username;
+
+	/**
+	 * Password of the user allowing to authenticate him.
+	 */
+	private String password;
+
+
+	/**
+	 * No arguments constructor. Water is wet.
+	 */
+	public User() {}
+
+
+	/**
+	 * All arguments constructor. The sky is blue.
+	 * @param username Username of the user to instantiate.
+	 * @param password Password of the user to instantiate.
+	 */
+	public User(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+
+	/**
+	 * Get the username of the User.
+	 * @return Username of the User.
+	 */
+	public String getUsername() {
+		return username;
+	}
+
+	/**
+	 * Set the username of the User.
+	 * @param username Username to set to the User.
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	/**
+	 * Get the password of the User.
+	 * @return Password of the User.
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * Set the password of the User.
+	 * @param password Password to set to the User.
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
