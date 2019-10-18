8b48c6340e45118762dc5e400c64f7a4a47093af
Camille Briand <camille.briand@efrei.net>
2019-10-18 18:18:54 +0200

    Make Authenticator class static and add Authenticator.isAuthenticated - The Authenticator class does not need to be instantiated. - The isAuthenticated static method allows to check whether a user is   authenticated thanks to its session data

--- src/main/java/efrei/m1/se/utils/Authenticator.java
+++ src/main/java/efrei/m1/se/utils/Authenticator.java
@@ -1,4 +1,21 @@
 package efrei.m1.se.utils;
 
+import javax.servlet.http.HttpServletRequest;
+
 public class Authenticator {
+	/**
+	 * Make the class "fully static"
+	 */
+	private Authenticator() {}
+
+	/**
+	 * Check whether or not a User is authenticated through its session data
+	 * @param req Incoming HTTP request (needed too find the session)
+	 * @return Whether the user is authenticated
+	 */
+	public static boolean isAuthenticated(HttpServletRequest req) {
+		String sessionUsername = (String) req.getSession().getAttribute("username");
+
+		return sessionUsername != null && !sessionUsername.isEmpty();
+	}
 }
