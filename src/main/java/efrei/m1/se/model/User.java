d0ad2ab084aff846f12efe98ebd8c59e0e4baafe
JulesLagny <jules.lagny@efrei.net>
2019-10-25 11:10:46 +0200

    Update User model

--- src/main/java/efrei/m1/se/model/User.java
+++ src/main/java/efrei/m1/se/model/User.java
@@ -1,69 +1,106 @@
 package efrei.m1.se.model;
 
 public class User {
+    private String name;
+    private String surname;
+    private String personalPhone;
+    private String mobilePhone;
+    private String workPhone;
+    private String adress;
+    private String postalCode;
+    private String city;
+    private String email;
+    private static int count = 0;
 
-	private static final String DEFAULT_USERNAME = "John Doe";
-	private static final String DEFAULT_PASSWORD = "password";
+    public User() {
+        name = "";
+        surname = "";
+        personalPhone = "";
+        mobilePhone = "";
+        workPhone = "";
+        adress = "";
+        postalCode = "";
+        city = "";
+        email = "";
+        count++;
+    }
 
-	/**
-	 * Unique username allowing to identify a user in the database.
-	 */
-	private String username;
+    public String getName() {
+        return this.name;
+    }
 
-	/**
-	 * Password of the user allowing to authenticate him.
-	 */
-	private String password;
+    public String getSurname() {
+        return this.surname;
+    }
 
+    public String getPersonalPhone() {
+        return this.personalPhone;
+    }
 
-	/**
-	 * No arguments constructor. Water is wet.
-	 */
-	public User() {
-		this.username = DEFAULT_USERNAME;
-		this.password = DEFAULT_PASSWORD;
-	}
+    public String getMobilePhone() {
+        return this.mobilePhone;
+    }
 
+    public String getWorkPhone() {
+        return this.workPhone;
+    }
 
-	/**
-	 * All arguments constructor. The sky is blue.
-	 * @param username Username of the user to instantiate.
-	 * @param password Password of the user to instantiate.
-	 */
-	public User(String username, String password) {
-		this.username = username;
-		this.password = password;
-	}
+    public String getAdress() {
+        return this.adress;
+    }
 
-	/**
-	 * Get the username of the User.
-	 * @return Username of the User.
-	 */
-	public String getUsername() {
-		return username;
-	}
+    public String getPostalCode() {
+        return this.postalCode;
+    }
 
-	/**
-	 * Set the username of the User.
-	 * @param username Username to set to the User.
-	 */
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    public String getCity() {
+        return this.postalCode;
+    }
 
-	/**
-	 * Get the password of the User.
-	 * @return Password of the User.
-	 */
-	public String getPassword() {
-		return password;
-	}
+    public String getEmail() {
+        return this.email;
+    }
 
-	/**
-	 * Set the password of the User.
-	 * @param password Password to set to the User.
-	 */
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setName(String nom) {
+        this.name = nom;
+    }
+
+    public void setSurname(String prenom) {
+        this.surname = prenom;
+    }
+
+    public void setPersonalPhone(String phone) {
+        this.personalPhone = phone;
+    }
+
+    public void setMobilePhone(String mobilePhone) {
+        this.mobilePhone = mobilePhone;
+    }
+
+    public void setWorkPhone(String workPhone) {
+        this.workPhone = workPhone;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String toString() {
+        return "USER " + count + " \n**IDENTITY** \n| Name : " + this.name + "\n| Surname : " + this.surname + "\n"
+                + "**PHONES** \n| Personal phone : " + personalPhone + "\n| Mobile phone " + mobilePhone + "\n| Work Phone : " + workPhone + "\n"
+                + "**ADRESS**\n| Street : " + adress + "\n| Code : " + postalCode + "\n| City : " + city + "\n"
+                + "**CONTACT** \n|Mail : " + email + "\n\n";
+    }
 }
