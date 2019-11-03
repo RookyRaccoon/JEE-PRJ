f6f2395cde9f814a531a86e7490b5eafe55ed54d
Camille Briand <camille.briand@efrei.net>
2019-11-03 13:17:00 +0100

    Add the DBInitFilter class

---
+++ src/main/java/efrei/m1/se/ctrl/DBInitFilter.java
@@ -0,0 +1,11 @@
+package efrei.m1.se.ctrl;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class DBInitFilter implements Filter {
+	@Override
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+	}
+}
