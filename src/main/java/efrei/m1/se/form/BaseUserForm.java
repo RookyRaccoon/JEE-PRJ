a76a5d110753de253c87b1fbde8c5df9cfb7beb4
Camille Briand <camille.briand@efrei.net>
2019-11-04 13:30:09 +0100

    Add BaseUserForm abstract class

---
+++ src/main/java/efrei/m1/se/form/BaseUserForm.java
@@ -0,0 +1,15 @@
+package efrei.m1.se.form;
+
+abstract class BaseUserForm {
+	protected static final String FRM_LASTNAME = "lastname";
+	protected static final String FRM_FIRSTNAME = "firstname";
+	protected static final String FRM_HOMEPHONE = "homephone";
+	protected static final String FRM_MOBILEPHONE = "mobilephone";
+	protected static final String FRM_WORKPHONE = "workphone";
+	protected static final String FRM_ADDRESS = "address";
+	protected static final String FRM_ZIPCODE = "zipcode";
+	protected static final String FRM_CITY = "city";
+	protected static final String FRM_EMAIL = "email";
+
+
+}
