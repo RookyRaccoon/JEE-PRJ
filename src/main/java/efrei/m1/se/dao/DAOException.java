c581dc1ca03cc9aec6a7e2e23b307cb44e6bec9d
Camille Briand <camille.briand@efrei.net>
2019-11-09 13:37:20 +0100

    Add efrei.m1.se.dao.DAOException exception

---
+++ src/main/java/efrei/m1/se/dao/DAOException.java
@@ -0,0 +1,15 @@
+package efrei.m1.se.dao;
+
+public class DAOException extends RuntimeException {
+	public DAOException(String message) {
+		super(message);
+	}
+
+	public DAOException(Throwable cause) {
+		super(cause);
+	}
+
+	public DAOException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
