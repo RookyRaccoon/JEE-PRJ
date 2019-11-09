b98047a12d4a75ee90ac4457e1e3aa84e13347e3
Camille Briand <camille.briand@efrei.net>
2019-11-09 13:39:28 +0100

    Add efrei.m1.se.dao.DAOFactory class and start to work on it

---
+++ src/main/java/efrei/m1/se/dao/DAOFactory.java
@@ -0,0 +1,11 @@
+package efrei.m1.se.dao;
+
+public class DAOFactory {
+	///////////////////////////////////////////////////////////////////////////
+	// STATIC FIELDS
+	///////////////////////////////////////////////////////////////////////////
+	private static final String PROP_URL = "jdbcUrl";
+	private static final String PROP_USER = "user";
+	private static final String PROP_PASSWORD = "password";
+	private static final String PROP_DRIVER = "driver";
+}
