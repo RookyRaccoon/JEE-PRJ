f9555962c5fc59ae2c5be1acb930389e332a3162
Camille Briand <camille.briand@efrei.net>
2019-11-09 16:28:23 +0100

    Add SQL Queries as private static constants of the EmployeeDAOImpl class

--- src/main/java/efrei/m1/se/dao/EmployeeDAOImpl.java
+++ src/main/java/efrei/m1/se/dao/EmployeeDAOImpl.java
@@ -12,6 +12,22 @@ import lombok.NonNull;
  */
 @AllArgsConstructor @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class EmployeeDAOImpl implements EmployeeDAO {
+
+	///region SQL Queries
+	///////////////////////////////////////////////////////////////////////////
+	// SQL Queries
+	///////////////////////////////////////////////////////////////////////////
+	private static final String SQL_SELECT_BY_ID = "SELECT ID, NAME, FIRSTNAME, HOMEPHONE, MOBILEPHONE, WORKPHONE, ADDRESS, POSTALCODE, CITY, EMAIL FROM Employees WHERE ID = ?";
+	private static final String SQL_INSERT_ONE   = "INSERT INTO Employees(NAME, FIRSTNAME, HOMEPHONE, MOBILEPHONE, WORKPHONE, ADDRESS, POSTALCODE, CITY, EMAIL) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+	private static final String SQL_DELETE_ONE   = "DELETE FROM Employees WHERE ID=?";
+	private static final String SQL_UPDATE_ONE   = "UPDATE Employees SET NAME = ?, FIRSTNAME = ?, HOMEPHONE = ?, MOBILEPHONE = ?, WORKPHONE = ?, ADDRESS = ?, POSTALCODE = ?, CITY = ?, EMAIL = ? WHERE ID = ?";
+	///endregion
+
+	/**
+	 * Needed to get connections to the database
+	 */
+	private DAOFactory daoFactory;
+
 	@Override
 	public void create(@NonNull User user) throws DAOException {
 
