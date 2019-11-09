eec252d8a8eb528d7da7a8fb5dae0549e7570ed3
Camille Briand <camille.briand@efrei.net>
2019-11-09 13:41:17 +0100

    Add all needed properties to DAOFactory class

--- src/main/java/efrei/m1/se/dao/DAOFactory.java
+++ src/main/java/efrei/m1/se/dao/DAOFactory.java
@@ -1,11 +1,23 @@
 package efrei.m1.se.dao;
 
+import com.zaxxer.hikari.HikariDataSource;
+
 public class DAOFactory {
+	///region Properties
 	///////////////////////////////////////////////////////////////////////////
-	// STATIC FIELDS
+	// STATIC PROPERTIES
 	///////////////////////////////////////////////////////////////////////////
 	private static final String PROP_URL = "jdbcUrl";
 	private static final String PROP_USER = "user";
 	private static final String PROP_PASSWORD = "password";
 	private static final String PROP_DRIVER = "driver";
+
+	///////////////////////////////////////////////////////////////////////////
+	// INSTANCE PROPERTIES
+	///////////////////////////////////////////////////////////////////////////
+	/**
+	 * Connection Pooling data source
+	 */
+	private HikariDataSource dataSource;
+	///endregion
 }
