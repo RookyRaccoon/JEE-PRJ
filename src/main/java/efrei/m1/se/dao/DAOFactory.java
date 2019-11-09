804decbb0b9d0f27aed51ce0fd50b5083a81a0a5
Camille Briand <camille.briand@efrei.net>
2019-11-09 14:11:18 +0100

    Add private constructor to DAOFactory class

--- src/main/java/efrei/m1/se/dao/DAOFactory.java
+++ src/main/java/efrei/m1/se/dao/DAOFactory.java
@@ -1,6 +1,8 @@
 package efrei.m1.se.dao;
 
+import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import lombok.NonNull;
 
 public class DAOFactory {
 	///region Properties
@@ -19,5 +21,23 @@ public class DAOFactory {
 	 * Connection Pooling data source
 	 */
 	private HikariDataSource dataSource;
+	///endregion
+
+
+	///region Initializers (constructor + getInstance)
+	///////////////////////////////////////////////////////////////////////////
+	// CONSTRUCTORS & INSTANTIATING METHODS
+	///////////////////////////////////////////////////////////////////////////
+
+	/**
+	 * Set the data source to use for connection pooling
+	 * <br /><br />
+	 * <i>This method is private to enforce use of Factory method pattern</i>
+	 * @param config Configuration of the data source
+	 */
+	private DAOFactory(@NonNull HikariConfig config) {
+		this.dataSource = new HikariDataSource(config);
+	}
+
 	///endregion
 }
