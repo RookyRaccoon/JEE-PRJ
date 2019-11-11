5b539420c35fa4a4180a380dec1ea99b84ce519d
Camille Briand <camille.briand@efrei.net>
2019-11-11 18:15:56 +0100

    Update User class to be JPA-Entity compatible

--- src/main/java/efrei/m1/se/model/User.java
+++ src/main/java/efrei/m1/se/model/User.java
@@ -8,9 +8,16 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.ArrayList;
 
 
+@Entity @Table(name = "Employees")
 @NoArgsConstructor @ToString
 public class User {
 
@@ -20,33 +27,43 @@ public class User {
 	@Getter @Setter
 	private static EmployeeDAO employeeDAO;
 
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY) @Column(name = "ID")
 	@Getter @Setter @ToString.Exclude
-	transient private String dbId;
+	private String dbId;
 
+	@Column(name = "NAME")
 	@Getter @Setter
 	private String name;
 
+	@Column(name = "FIRSTNAME")
 	@Getter @Setter
 	private String surname;
 
+	@Column(name = "HOMEPHONE")
 	@Getter @Setter
 	private String personalPhone;
 
+	@Column(name = "MOBILEPHONE")
 	@Getter @Setter
 	private String mobilePhone;
 
+	@Column(name = "WORKPHONE")
 	@Getter @Setter
 	private String workPhone;
 
+	@Column(name = "ADDRESS")
 	@Getter @Setter
 	private String address;
 
+	@Column(name = "POSTALCODE")
 	@Getter @Setter
 	private String postalCode;
 
+	@Column(name = "CITY")
 	@Getter @Setter
 	private String city;
 
+	@Column(name = "EMAIL")
 	@Getter @Setter
 	private String email;
 
