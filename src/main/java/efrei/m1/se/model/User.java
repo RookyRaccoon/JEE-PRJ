e24b12605e7af3ba3bf2c97a93e083645d7b1299
Camille Briand <camille.briand@efrei.net>
2019-11-11 19:21:15 +0100

    User class cleanup

--- src/main/java/efrei/m1/se/model/User.java
+++ src/main/java/efrei/m1/se/model/User.java
@@ -1,6 +1,5 @@
 package efrei.m1.se.model;
 
-import efrei.m1.se.dao.DAOException;
 import efrei.m1.se.dao.EmployeeDAO;
 
 import lombok.Getter;
@@ -14,7 +13,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import java.util.ArrayList;
 
 
 @Entity @Table(name = "Employees")
@@ -77,66 +75,6 @@ public class User {
 		this.postalCode = postalCode;
 		this.city = city;
 		this.email = email;
-		this.dbId = "";
-	}
-
-	/**
-	 * Retrieve a single {@link User} from the database based upon its {@code id}
-	 * @param id Id of the {@link User} to retrieve from the database
-	 * @return {@link User} object for the matching database record
-	 */
-	public static User withId(String id) {
-		// No need to perform a DB read if no id was provided
-		if (id == null) {
-			return null;
-		}
-
-		try {
-			return User.employeeDAO.findById(id);
-		} catch (DAOException ignore) {}
-
-		return null;
-	}
-
-	/**
-	 * Create a user record in the database
-	 */
-	public void createRecord() {
-		try {
-			User.employeeDAO.create(this);
-		} catch (DAOException ignore) {}
-	}
-
-
-	/**
-	 * Get all users from the database as an {@link ArrayList} of {@link User}. Water is wet.
-	 * @return {@link ArrayList} of {@link User}
-	 */
-	public static ArrayList<User> getAllUsers() {
-		return User.employeeDAO.findAll();
-	}
-
-
-	/**
-	 * Securely handles the deletion of a User record in the database
-	 * @param id Id of the user to delete
-	 */
-	public static void deleteRecord(String id) {
-		try {
-			User u = User.employeeDAO.findById(id);
-			if (u != null) {
-				User.employeeDAO.delete(u);
-			}
-		} catch (DAOException ignore) {}
-	}
-
-
-	/**
-	 * Update a {@link User} record in the database
-	 */
-	public void updateRecord() {
-		try {
-			User.employeeDAO.update(this);
-		} catch (DAOException ignore) {}
+		this.dbId = null;
 	}
 }
